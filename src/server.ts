9da685889f91f8e8a028711cb59cf65a9797665e
Naresh <nareshsu427@gmail.com>
2024-07-08 19:16:36 +0530

    add logger

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,19 @@
 import app from "./app";
 import { Config } from "./config";
+import logger from "./config/logger";
 
 const startServer = () => {
     const PORT = Config.PORT;
     try {
         app.listen(PORT, () => {
             // eslint-disable-next-line no-console
-            console.log(`Server running on port ${PORT}`);
+            logger.info(`Server running on port ${PORT}`);
         });
-    } catch (error) {
-        // eslint-disable-next-line no-console
-        console.log(error);
-        process.exit(1);
+    } catch (err: unknown) {
+        if (err instanceof Error) {
+            logger.error(err.message);
+            process.exit(1);
+        }
     }
 };
 
