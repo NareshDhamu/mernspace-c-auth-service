31219f1f7ebdb0b4c531f416c97733823edda2c7
Naresh <nareshsu427@gmail.com>
2024-07-10 16:05:24 +0530

    write first test

--- tests/users/register.spec.ts
+++ tests/users/register.spec.ts
@@ -20,6 +20,38 @@ describe("POST /auth/register", () => {
             // Assert
             expect(response.statusCode).toBe(201);
         });
+        it("should return valid json response", async () => {
+            // AAA
+            // Arrange
+            const userData = {
+                firstName: "Naresh",
+                lastName: "Dhamu",
+                email: "nareshDhamu@gmail.com",
+                password: "naresh123",
+            };
+            // Act
+
+            const response = await request(app)
+                .post("/auth/register")
+                .send(userData);
+            // Assert
+            expect(
+                (response.headers as Record<string, string>)["content-type"],
+            ).toEqual(expect.stringContaining("json"));
+        });
+        it("should persist the user in the database", async () => {
+            // AAA
+            // Arrange
+            const userData = {
+                firstName: "Naresh",
+                lastName: "Dhamu",
+                email: "nareshDhamu@gmail.com",
+                password: "naresh123",
+            };
+            // Act
+            await request(app).post("/auth/register").send(userData);
+            // Assert
+        });
     });
     describe("Fields are missing", () => {});
 });
